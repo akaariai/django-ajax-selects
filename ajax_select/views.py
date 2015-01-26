6bf0e2f67707cf70c80a100ca0f8fc03a827c33b
Anssi Kääriäinen <akaariai@gmail.com>
2015-01-26 14:39:45 +0200

    Revert "Merge branch 'master' into local_mods"
    This reverts commit 65bfdf04c6f88e767fcd2826b6c22c824ff72aa9, reversing
    changes made to 73b80f58bcb0057e49aea94efadc32085ca223b1.

--- ajax_select/views.py
+++ ajax_select/views.py
@@ -3,14 +3,10 @@ from ajax_select import get_lookup
 from django.contrib.admin import site
 from django.db import models
 from django.http import HttpResponse
-try:
-    import json
-except ImportError:
-    from django.utils import simplejson as json
-from django.utils.encoding import force_text
+import json as simplejson
 
 
-def ajax_lookup(request, channel):
+def ajax_lookup(request,channel):
 
     """ this view supplies results for foreign keys and many to many fields """
 
@@ -23,58 +19,50 @@ def ajax_lookup(request, channel):
         query = request.GET['term']
     else:
         if 'term' not in request.POST:
-            return HttpResponse('')  # suspicious
+            return HttpResponse('') # suspicious
         query = request.POST['term']
 
     lookup = get_lookup(channel)
-    if hasattr(lookup, 'check_auth'):
+    if hasattr(lookup,'check_auth'):
         lookup.check_auth(request)
 
     if len(query) >= getattr(lookup, 'min_length', 1):
-        instances = lookup.get_query(query, request)
+        instances = lookup.get_query(query,request)
     else:
         instances = []
 
-    results = json.dumps([
+    results = simplejson.dumps([
         {
-            'pk': force_text(getattr(item, 'pk', None)),
+            'pk': unicode(getattr(item,'pk',None)),
             'value': lookup.get_result(item),
-            'match': lookup.format_match(item),
+            'match' : lookup.format_match(item),
             'repr': lookup.format_item_display(item)
         } for item in instances
     ])
 
-    return HttpResponse(results, content_type='application/javascript')
+    return HttpResponse(results, mimetype='application/javascript')
 
 
-def add_popup(request, app_label, model):
+def add_popup(request,app_label,model):
     """ this presents the admin site popup add view (when you click the green +)
 
         make sure that you have added ajax_select.urls to your urls.py:
             (r'^ajax_select/', include('ajax_select.urls')),
         this URL is expected in the code below, so it won't work under a different path
-
+        
         this view then hijacks the result that the django admin returns
-        and instead of calling django's dismissAddAnontherPopup(win,newId,newRepr)
+        and instead of calling django's dismissAddAnontherPopup(win,newId,newRepr) 
         it calls didAddPopup(win,newId,newRepr) which was added inline with bootstrap.html
     """
     themodel = models.get_model(app_label, model)
     admin = site._registry[themodel]
 
     # TODO : should detect where we really are
-    admin.admin_site.root_path = "/ajax_select/"
+    admin.admin_site.root_path = "/ajax_select/" 
 
-    response = admin.add_view(request, request.path)
+    response = admin.add_view(request,request.path)
     if request.method == 'POST':
-        try:
-            # this detects TemplateResponse which are not yet rendered
-            # and are returned for form validation errors
-            if not response.is_rendered:
-                out = response.rendered_content
-            else:
-                out = response.content
-        except AttributeError:  # django < 1.5
-            out = response.content
-        if 'opener.dismissAddAnotherPopup' in out:
-            return HttpResponse(out.replace('dismissAddAnotherPopup', 'didAddPopup'))
+        if 'opener.dismissAddAnotherPopup' in response.content:
+            return HttpResponse( response.content.replace('dismissAddAnotherPopup','didAddPopup' ) )
     return response
+
